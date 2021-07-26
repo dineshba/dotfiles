b365be3c385693e6901db3f7a608259a3eaefe57
Dinesh <dineshudt17@gmail.com>
2021-07-26 10:18:13 +0530

    Remove not working code for hiding application in hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,18 +5,7 @@ hs.hotkey.bind(hyperKey, "r", function()
 end)
 
 hs.hotkey.bind(hyperKey, "space", function()
-  local app = hs.application.get("alacritty")
-    if app then
-        if not app:mainWindow() then
-            app:selectMenuItem({"Alacritty", "New OS window"})
-        elseif app:isFrontmost() then
-            app:hide()
-        else
-            app:activate()
-        end
-    else
-        hs.application.launchOrFocus("/Applications/Alacritty.app")
-    end
+  hs.application.launchOrFocus("/Applications/Alacritty.app")
 end)
 
 hs.hotkey.bind(hyperKey, "z", function()
